f0d6c1b08aadee278db7453514db0a6fdb06eb4e
Daniel Parra <danialepaco@hotmail.com>
2016-06-09 10:45:57 -0400

    Initial Commit

---
+++ favorite_place/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  favorite_place
+//
+//  Created by Daniel Parra on 6/9/16.
+//  Copyright © 2016 Daniel Parra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
